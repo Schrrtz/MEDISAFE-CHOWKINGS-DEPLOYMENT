1359d76205ef037530ba597ae631a7aca817dcb8
Schrrtz <clynetayona@gmail.com>
2025-11-26 14:47:51 +0800

    Remove migrations from startup to avoid connection issues

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Running migrations..."
-python manage.py migrate --noinput
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
 echo "Starting gunicorn..."
-gunicorn --pythonpath PBL --bind 0.0.0.0:$PORT MEDISAFE_PBL.wsgi:application
+exec gunicorn --pythonpath PBL --bind 0.0.0.0:$PORT MEDISAFE_PBL.wsgi:application
